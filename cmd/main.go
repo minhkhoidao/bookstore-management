cc42c12f9297053d19458a8c03f4ec3704d42387
khoidaoo <minhkhoi@telio.vn>
2024-03-28 11:07:06 +0700

    add users services

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"bookstore/pkg/container"
+	"bookstore/pkg/routes"
+	"fmt"
+)
+
+func InitContainer() {
+	initContainer := container.NewContainer()
+	userService := initContainer.UserService()
+
+	router := routes.UserRoutes(userService)
+
+	router.Run(":8080")
+}
 
 func main() {
 	fmt.Println("Hello, World!")
+	InitContainer()
+
 }
